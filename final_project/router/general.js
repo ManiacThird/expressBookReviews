b9aa902334d6ad9655399a23f4daf6378f11eb48
ManiacThird <ozcar_00@hotmail.com>
2025-10-24 17:20:53 -0400

    Tarea 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -34,10 +34,23 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    let result = [];
+
+    Object.keys(books).forEach((key) => {
+        if (books[key].author === author) {
+            result.push(books[key]);
+        }
+    });
+
+    if (result.length > 0) {
+        return res.status(200).json(result);
+    } else {
+        return res.status(404).json({ message: "No se encontraron libros para este autor" });
+    }
 });
+
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
