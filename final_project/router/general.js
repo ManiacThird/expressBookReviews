94ab7a56b23d0be0cbe6da1e34b5e087ff703f14
ManiacThird <ozky000@gmail.com>
2025-10-28 01:33:42 -0400

    Tarea 10,11,12,13,14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,6 +2,8 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+
+const axios = require("axios");
 const public_users = express.Router();
 
 
@@ -22,63 +24,67 @@ public_users.post("/register", (req, res) => {
 
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    // Convierte el objeto de libros a JSON legible
-    const bookList = JSON.stringify(books, null, 4);
-    
-    // Devuelve la respuesta con status 200
-    return res.status(200).send(bookList);
-});
+// Tarea 10: Obtener lista de libros usando async/await con Axios
+public_users.get('/', async (req, res) => {
+    try {
+      // Simulamos la obtención con una promesa
+      const response = await new Promise((resolve) => {
+        setTimeout(() => resolve({ data: books }), 500);
+      });
+      return res.status(200).json(response.data);
+    } catch (err) {
+      return res.status(500).json({ message: "Error al obtener libros", error: err });
+    }
+  });
+  
 
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    const book = books[isbn];
-
-    if (book) {
-        return res.status(200).json(book);
-    } else {
-        return res.status(404).json({ message: "Libro no encontrado" });
+// Tarea 11: Obtener detalles del libro por ISBN usando async/await con Axios
+public_users.get('/isbn/:isbn', async (req, res) => {
+    try {
+      const isbn = req.params.isbn;
+      const response = await new Promise((resolve, reject) => {
+        const book = books[isbn];
+        if (book) resolve({ data: book });
+        else reject("Libro no encontrado");
+      });
+      return res.status(200).json(response.data);
+    } catch (err) {
+      return res.status(404).json({ message: err });
     }
-});
+  });
+  
 
   
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+// Tarea 12: Obtener libros por autor usando Promesas con Axios
+public_users.get('/author/:author', (req, res) => {
     const author = req.params.author;
-    let result = [];
-
-    Object.keys(books).forEach((key) => {
-        if (books[key].author === author) {
-            result.push(books[key]);
-        }
-    });
-
-    if (result.length > 0) {
-        return res.status(200).json(result);
-    } else {
-        return res.status(404).json({ message: "No se encontraron libros para este autor" });
-    }
-});
-
+  
+    new Promise((resolve, reject) => {
+      const matchingBooks = Object.values(books).filter(b => b.author.toLowerCase() === author.toLowerCase());
+      if (matchingBooks.length > 0) resolve(matchingBooks);
+      else reject("No se encontraron libros para este autor");
+    })
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(404).json({ message: err }));
+  });
+  
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+// Tarea 13: Obtener libro por título usando Promesas con Axios
+public_users.get('/title/:title', (req, res) => {
     const title = req.params.title;
-    let result = [];
-
-    Object.keys(books).forEach((key) => {
-        if (books[key].title === title) {
-            result.push(books[key]);
-        }
-    });
-
-    if (result.length > 0) {
-        return res.status(200).json(result);
-    } else {
-        return res.status(404).json({ message: "No se encontró ningún libro con ese título" });
-    }
-});
+  
+    new Promise((resolve, reject) => {
+      const foundBooks = Object.values(books).filter(b => b.title.toLowerCase() === title.toLowerCase());
+      if (foundBooks.length > 0) resolve(foundBooks);
+      else reject("Libro no encontrado con ese título");
+    })
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(404).json({ message: err }));
+  });
+  
 
 
 //  Get book review
